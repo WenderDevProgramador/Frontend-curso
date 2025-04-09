b5e55677d9f2afaa932afd15072f8ff72e245535
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2025-04-09 19:19:57 -0300

    add

---
+++ React/Aulas/Aula1/index.jsx
@@ -0,0 +1,12 @@
+function Button() {
+    return <button>Meu Botão</button>
+}
+
+function Container() {
+    return (
+        <div className="">
+            <h1>Titulo</h1>
+            <p>Texto</p>
+        </div>
+    )
+}
