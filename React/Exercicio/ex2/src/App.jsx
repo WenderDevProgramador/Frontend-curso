d3d3eec46913d817f3aca3907b3367f5bf41e898
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2025-04-15 18:52:37 -0300

    Add

--- React/Exercicio/ex2/src/App.jsx
+++ React/Exercicio/ex2/src/App.jsx
@@ -1,16 +1,26 @@
 
 import Card from "./components/Card"
+import starImg from './assets/star.jpg'
+import returnImg from './assets/return.png'
+import empireImg from './assets/empire.png'
+import Button from "./components/Button"
+
+
 
 export default function App() {
   return (
     <>
       <h1>Exercício</h1>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <Button text='Ir ao Blog'/>
+      <br />
+      <Button text='Sair da pagina'/>
+      <Card title="Pôster: Star Ears (1977)" posterImg={starImg}/>
+      <Card title="Pôster: Empire Stikes Back (1980)" posterImg={empireImg}/>
+      <Card title="Pôster: Return of the Jedi (1983)" posterImg={returnImg}/>
+
+      
+      
+  
     </>
 
   )
