846e5fbbd5d2682024ac2d70ba28f2369a067929
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2025-05-04 11:59:37 -0300

    Aula 10

---
+++ React/Aulas/Aula10/src/router.jsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter  } from 'react-router-dom'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import Cart from './pages/Cart'
+import Admin from './pages/Admin'
+
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home/>
+    },
+    {
+        path: '/products',
+        element: <Products/>
+    },
+    {
+        path: '/cart',
+        element: <Cart/>
+    },
+    {
+        path:'/admin',
+        element: <Admin/>
+    }
+
+])
+
+export default router
