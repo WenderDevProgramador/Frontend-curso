a457b87bd8370ba94a4613d256e55f6601dd8f87
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2025-05-28 19:15:00 -0300

    add

--- Raact2/Aulas/aula4/src/App.jsx
+++ Raact2/Aulas/aula4/src/App.jsx
@@ -1,33 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Title from './component/Title'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [num , setNum] = useState(2)
+  const [red , setRed] = useState(true)
+  
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2 style={num > 10 ? {color:'red'} : {color:'magenta'}}>Css inline dinamico</h2>
+        <button onClick={() => setNum(2)}>Menor que 10</button> <br />
+        <button onClick={() => setNum(22)}>Maior que 10</button>
+        <h3>Titulo é igual á : {num} </h3>
+        {/* Classe dinamica */}
+
+        <h2 className={red ? 'red-title' : 'blue-title'}>Classe dinamica</h2>
+        <button onClick={() => setRed(true)}>Vermelho</button> <br />
+        <button onClick={() => setRed(false)}>Azul</button>
+        {/* Css modules */}
+        <Title/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        
     </>
   )
 }
---
+++ Raact2/Aulas/aula4/src/component/Title.jsx
@@ -0,0 +1,12 @@
+import classes from './Title.module.css'
+
+const Title = () => {
+    return (
+        <>
+            <h1 className={classes.title}>Meu título!</h1>
+            <h1 className={classes.tit}>Meu título!</h1>
+        </>
+    )
+}
+
+export default Title
