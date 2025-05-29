a4483e88f7563871ea8b71753cfe03b0f577ff2d
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2025-05-29 19:03:17 -0300

    Base projeto criada

---
+++ Projetos-Front-End/ProjetoCalcImc/src/data/data.js
@@ -0,0 +1,37 @@
+export const data = [
+    {
+        min: 0,
+        max: 18.4,
+        classificacao: 'Magreza',
+        info: 'Menor que 18,5',
+        infoClass: 'low'
+    },
+    {
+        min: 18.5,
+        max: 24.9,
+        classificacao: 'Normal',
+        info: 'Entre 18,5 e 24,9',
+        infoClass: 'good'
+    },
+    {
+        min: 25,
+        max: 29.9,
+        classificacao: 'Sobrepeso',
+        info: 'Entre 25 e 29,9',
+        infoClass: 'medium'
+    },
+    {
+        min: 30,
+        max: 39.9,
+        classificacao: 'Obesidade',
+        info: 'Entre 30 e 39,9',
+        infoClass: 'high'
+    },
+    {
+        min: 40,
+        max: Infinity,
+        classificacao: 'Obesidade Grave',
+        info: 'Maior que 40',
+        infoClass: 'very-high'
+    }
+];
