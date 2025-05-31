2fa57ac30cd1546952ca318f85b88cc133ff03f4
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2025-05-31 18:31:55 -0300

    Projeto Finalizado

--- Projetos-Front-End/ProjetoCalcImc/src/components/ImcCalc.jsx
+++ Projetos-Front-End/ProjetoCalcImc/src/components/ImcCalc.jsx
@@ -3,9 +3,29 @@ import { useState } from "react"
 import './ImcCalc.css'
 
 
-const ImcCalc = () => {
+const ImcCalc = ({calcImc}) => {
     const [height, setHeight] = useState('')
     const [weight, setWeight] = useState('')
+
+    const clearForm = (e) => {
+        e.preventDefault()
+        setWeight('')
+        setHeight('')
+    }
+
+    const validDigits = (text) => {
+        return text.replace(/[^0-9,]/g, '')
+    }
+
+    const handleHeightCharge = (e) => {
+        const updateValue = validDigits(e.target.value)
+        setHeight(updateValue)
+    }
+
+    const handleWeightCharge = (e) => {
+        const updateValue = validDigits(e.target.value)
+        setWeight(updateValue)
+    }
 
 
     return (
@@ -15,17 +35,30 @@ const ImcCalc = () => {
                 <div className="form-inputs">
                     <div className="form-control">
                         <label htmlFor="height">Altura</label>
-                        <input type="text" name="height" id="height" placeholder="Exemplo 1,75" />
+                        <input 
+                        type="text" 
+                        name="height" 
+                        id="height" 
+                        placeholder="Exemplo 1,75"
+                        onChange={handleHeightCharge}
+                        value={height} />
                     </div>
 
                     <div className="form-control">
                         <label htmlFor="weight">Peso</label>
-                        <input type="text" name="weight" id="weight" placeholder="Exemplo 70,5" />
+                        <input 
+                        type="text" 
+                        name="weight" 
+                        id="weight" 
+                        placeholder="Exemplo 70,5"
+                        onChange={handleWeightCharge}
+                        value={weight} />
                     </div>
                 </div>
                 <div className="action-control">
-                    <Button id='calc-btn' text='Calcular' />
-                    <Button id='clear-btn' text='Limpar' />
+                    <Button id='calc-btn' text='Calcular' 
+                    action={(e) => calcImc(e, height, weight)} />
+                    <Button id='clear-btn' text='Limpar' action={clearForm} />
 
                 </div>
             </form>
--- Projetos-Front-End/ProjetoCalcImc/src/data/data.js
+++ Projetos-Front-End/ProjetoCalcImc/src/data/data.js
@@ -2,36 +2,41 @@ export const data = [
     {
         min: 0,
         max: 18.4,
-        classificacao: 'Magreza',
-        info: 'Menor que 18,5',
-        infoClass: 'low'
+        info: 'Magreza',
+        classification: 'Menor que 18,5',
+        infoClass: 'low',
+        obesity: '0'
     },
     {
         min: 18.5,
         max: 24.9,
-        classificacao: 'Normal',
-        info: 'Entre 18,5 e 24,9',
-        infoClass: 'good'
+        info: 'Normal',
+        classification: 'Entre 18,5 e 24,9',
+        infoClass: 'good',
+        obesity: '0'
     },
     {
         min: 25,
         max: 29.9,
-        classificacao: 'Sobrepeso',
-        info: 'Entre 25 e 29,9',
-        infoClass: 'medium'
+        info: 'Sobrepeso',
+        classification: 'Entre 25 e 29,9',
+        infoClass: 'medium',
+        obesity: 'I'
     },
     {
         min: 30,
         max: 39.9,
-        classificacao: 'Obesidade',
-        info: 'Entre 30 e 39,9',
-        infoClass: 'high'
+        info: 'Obesidade',
+        classification: 'Entre 30 e 39,9',
+        infoClass: 'high',
+        obesity: 'II'
     },
     {
         min: 40,
         max: Infinity,
-        classificacao: 'Obesidade Grave',
-        info: 'Maior que 40',
-        infoClass: 'very-high'
+        info: 'Obesidade Grave',
+        classification: 'Maior que 40',
+        infoClass: 'very-high',
+        obesity: 'III'
     }
 ];
