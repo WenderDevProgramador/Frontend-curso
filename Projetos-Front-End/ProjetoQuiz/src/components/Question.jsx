e37122cf062f73fdf4ee71e62e696fe3a72cb7e7
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2025-06-24 19:22:21 -0300

    app

--- Projetos-Front-End/ProjetoQuiz/src/components/Question.jsx
+++ Projetos-Front-End/ProjetoQuiz/src/components/Question.jsx
@@ -2,7 +2,7 @@ import { useContext } from 'react'
 import { QuizContext } from '../context/quiz'
 
 
-
+import Options from './Options'
 
 import './Question.css'
 
@@ -12,16 +12,36 @@ const Question = () => {
 
     const currentQuestion = quizState.questions[quizState.currentQuestion]
 
-    
+    const onSelectOption = (option) => {
+        dispatch({
+            type: 'CHEK_ANSWER',
+            payload: {answer: currentQuestion.answer, option},
+
+        })
+    }
+
+
 
     return (
         <div id='question'>
             <p>Pergunta {quizState.currentQuestion + 1} de {quizState.questions.length}</p>
             <h2>{currentQuestion.question}</h2>
             <div id="options-container">
-                <p>Opções</p>
+                <div>
+                    {currentQuestion.options.map((option) => (
+                        <Options
+                            option={option}
+                            key={option}
+                            answer={currentQuestion.answer}
+                            selectOption={() => onSelectOption(option)}
+                        />
+                    ))}
+                </div>
             </div>
-            <button onClick={() => dispatch({type: 'CHANGE_QUESTION'})}>Continuar</button>
+
+            {quizState.answerSelected && (
+                <button onClick={() => dispatch({ type: 'CHANGE_QUESTION' })}>Continuar</button>
+            )}
         </div>
     )
 }
