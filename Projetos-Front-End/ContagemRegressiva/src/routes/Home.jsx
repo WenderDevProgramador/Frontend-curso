ebb47f71ab5c5276f705f2220a5d6030a2793052
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2025-07-03 19:07:22 -0300

    add

--- Projetos-Front-End/ContagemRegressiva/src/routes/Home.jsx
+++ Projetos-Front-End/ContagemRegressiva/src/routes/Home.jsx
@@ -1,9 +1,66 @@
+import { useState } from 'react'
 
+import './home.css'
 
 const Home = () => {
+
+    const [title, setTitle] = useState('')
+    const [date, setDate] = useState('')
+    const [image, setImage] = useState('')
+    const [color, setColor] = useState('')
+
+    const handleSubmit = (ev) => {
+        ev.preventDefault()
+
+        const eventObjetct = {
+            title,
+            date,
+            image,
+            color,
+    }
+
+    console.log(eventObjetct)
+
+}
+
+
     return (
         <div className="home">
-            Home
+            <h2>Monte a sua contagem regressiva!</h2>
+            <form className="countdown-form" onSubmit={handleSubmit}>
+                <label >
+                    <span>Título:</span>
+                    <input
+                        type="text"
+                        name="title"
+                        placeholder="Digite o título"
+                        required
+                        onChange={(ev) => setTitle(ev.target.value)} />
+                </label>
+
+                <label >
+                    <span>Data:</span>
+                    <input type="date" name="date" required onChange={(ev) => setDate(ev.target.value)} />
+                </label>
+
+                <label >
+                    <span>Imagem:</span>
+                    <input 
+                    type="text" 
+                    name="image" 
+                    placeholder="Insira a url da imagem"  
+                    onChange={(ev) => setImage(ev.target.value)} />
+                </label>
+
+                <label >
+                    <span>Cor do tema:</span>
+                    <input type="color"
+                        name="color"
+                        onChange={(ev) => setColor(ev.target.value)}
+                        required />
+                </label>
+                <input type="submit" value="Enviar" />
+            </form>
         </div>
     )
 }
